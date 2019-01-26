d2868623f5dcd0cd1f0b664e9de268f92a7f6d79
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-01-26 12:20:28 -0500

    test model info rendered onto index and show pages, pages linked together

---
+++ models/cats.js
@@ -0,0 +1,22 @@
+
+
+const cats = [
+  {
+   name: 'Mittens',
+   favoriteFood: 'Fancy Feast',
+   owner: 'Maude'
+  },
+  {
+    name: 'Rover',
+    favoriteFood: 'Tender Vittles',
+    owner: 'Maude'
+  },
+  {
+    name: 'Charley',
+    favoriteFood: 'Friskies',
+    owner: 'Jane'
+  }
+];
+
+
+module.exports = cats
--- server.js
+++ server.js
@@ -4,28 +4,42 @@
 const express = require('express')
 const app = express();
 const port = 3000;
-
+const cats = require('./models/cats.js');
 
 // =======================================
 // <<<<<<<<<<<<MIDDLEWARE>>>>>>>>>>>>>>
 // =======================================
 
-
-
+// =======================================
+// <<<<<<<7 RESTFUL ROUTES>>>>>>>>>>>>>
+// =======================================
+// URL	              HTTP Verb	   Action
+// /cats/	        GET	         index
+// /cats/new	      GET	         new
+// /cats	          POST	       create
+// /cats/:id	      GET	         show
+// /cats/:id/edit	GET	         edit
+// /cats/:id	      PATCH/PUT	   update
+// /cats/:id       DELETE	     destroy
 
 // =======================================
 // <<<<<<<<<<<<INDEX ROUTE>>>>>>>>>>>>>>>
 // =======================================
-app.get('/',(req,res) => {
-  res.render('index.ejs')
-})
+app.get('/cutecats',(req,res) => {
+  res.render('index.ejs',{
+    allCats: cats
+  });
+});
 
 // =======================================
 // <<<<<<<<<<<<SHOW ROUTE>>>>>>>>>>>>>>>
 // =======================================
-app.get('/:id',(req,res) => {
-  res.render('show.ejs')
-})
+app.get('/cutecats/:id',(req,res) => {
+  res.render('show.ejs',
+  {
+    cat: cats[req.params.id]
+  });
+});
 
 
 // =======================================
