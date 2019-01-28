872d0d528620d7342ee140b953ee3751ba22101b
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-01-28 13:08:20 -0500

    cats - connected users controllers, models, and views

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const app = express();
 const methodOverride = require('method-override');
 const catsController = require('./controllers/cats.js')
+const userController = require('./controllers/users.js')
 const db = mongoose.connection;
 // =======================================
 // <<<<<<<<<<<PORT>>>>>>>>>>>>>>
@@ -40,20 +41,8 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
-app.use( catsController);
-
-// =======================================
-// <<<<<<<7 RESTFUL ROUTES>>>>>>>>>>>>>
-// =======================================
-// URL	             HTTP Verb	   Action     Included
-// /cats/	           GET	         index      XX
-// /cats/new	       GET	         new        XX
-// /cats	           POST	         create     XX
-// /cats/:id	       GET	         show       XX
-// /cats/:id/edit	   GET	         edit       XX
-// /cats/:id	       PATCH/PUT	   update     XX
-// /cats/:id         DELETE	       destroy    XX
-
+app.use(catsController);
+app.use('/users', userController);
 
 
 // =======================================
